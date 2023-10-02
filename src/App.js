b53be05a67bf9f70726793b9681c32cdfcc717f9
0xSarkar <0xsarkar@gmail.com>
2023-10-02 14:46:02 +0530

    exporting and sing Modal object instead of individually exporting Modal child components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 
 import Modal from "./components/Modal";
-import ModalBody from "./components/Modal/ModalBody";
-import ModalHeader from "./components/Modal/ModalHeader";
-import ModalFooter from "./components/Modal/ModalFooter";
 import Button from "./components/Button/Button";
-import ModalBox from "./components/Modal/ModalBox";
-import ModalBackdrop from "./components/Modal/ModalBackdrop";
 import { ThemeProvider } from "./contexts";
 import { themeOptions } from "./theme";
 
@@ -46,23 +41,23 @@ function App() {
       </div>
     </div>
 
-    <ModalBackdrop isOpen={modalIsOpen} setIsOpen={setModalIsOpen}>
-      <ModalBox>
-        <ModalHeader>Awesome Modal Header</ModalHeader>
-        <ModalBody>
+    <Modal.BackDrop isOpen={modalIsOpen} setIsOpen={setModalIsOpen}>
+      <Modal.Box>
+        <Modal.Header>Awesome Modal Header</Modal.Header>
+        <Modal.Body>
           <p>
             Modal's body text goes here...
           </p>
           <p className="mt-2">
             The default width of modals is `max-w-md`. You can update it by passing your desired width class to the `ModalBox` component.
           </p>
-        </ModalBody>
-        <ModalFooter>
+        </Modal.Body>
+        <Modal.Footer>
           <Button variant="boxed" onClick={() => setModalIsOpen(false)}>Close</Button>
           <Button className="ml-2">Save Updates</Button>
-        </ModalFooter>
-      </ModalBox>
-    </ModalBackdrop>
+        </Modal.Footer>
+      </Modal.Box>
+    </Modal.BackDrop>
 
     </ThemeProvider>
     </>
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,15 @@
-import Modal from "./ModalBox";
+import ModalBackdrop from "./ModalBackdrop";
+import ModalBody from "./ModalBody";
+import ModalBox from "./ModalBox";
+import ModalFooter from "./ModalFooter";
+import ModalHeader from "./ModalHeader";
+
+const Modal = {
+  BackDrop: ModalBackdrop,
+  Box: ModalBox,
+  Header: ModalHeader,
+  Body: ModalBody,
+  Footer: ModalFooter,
+}
 
 export default Modal
